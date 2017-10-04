611dec8817db6c924782584caed8d3563df50a35
Steve Bronder <stevo15025@gmail.com>
2017-10-04 00:36:28 -0400

    update with addStock(), still wip

--- R/instruments.R
+++ R/instruments.R
@@ -1,3 +1,5 @@
+instrument_all = new.env()
+
 instruments = R6Class("instruments",
                       lock_objects = FALSE,
                       public = list(
@@ -15,25 +17,62 @@ instruments = R6Class("instruments",
                           self$options = options
                           self$stocks = stocks
                         },
-                        addCurrency = function(...) {
-                          self$currencies <- c(self$currencies, currency$new(...))
+                        dep_sub = function(x) deparse(substitute(x)),
+                        addCurrency = function(primary_id, ...) {
+                          self$currencies <- c(self$currencies,
+                                               currency$new(primary_id, ...))
                           names(self$currencies) <- unlist(lapply(self$currencies,
                                                                 function(x) x$primary_id))
                         },
                         getCurrency = function(primary_id) {
-                          return(self$currencies[[deparse(substitute(primary_id))]])
+                          return(self$currencies[[self$dep_sub(primary_id)]])
                         },
-                        addStock = function(...){
-                          self$stock <- c(self$stock, stock$new(...))
-                          names(self$stock) <- unlist(lapply(self$stock,
-                                                                  function(x) x$primary_id))
+                        addStock = function(primary_id, currency, ...){
+                          currency = self$getCurrency(currency)
+                          self$stocks <- c(self$stocks,
+                                          stock$new(primary_id, currency, ...)
+                                          )
+                          names(self$stocks) <- unlist(
+                            lapply(self$stocks, function(x) x$primary_id)
+                            )
                         },
                         getStock = function(primary_id) {
-                          return(self$stock[[deparse(substitute(primary_id))]])
+                          return(self$stock[[self$dep_sub(primary_id)]])
                         }
-                      )
+                      ),
+                      parent_env = instrument_all
                       )
 
+#' constructor for spot exchange rate instruments
+#'
+#' Currency symbols (like any symbol) may be any combination of alphanumeric
+#' characters, but the FX market has a convention that says that the first
+#' currency in a currency pair is the 'target'  and the second currency in the
+#' symbol pair is the currency the rate ticks in.  So 'EURUSD' can be read as
+#' 'USD per 1 EUR'.
+#'
+#' In \code{FinancialInstrument} the \code{currency} of the instrument should
+#' be the currency that the spot rate ticks in, so it will typically be the
+#' second currency listed in the symbol.
+#'
+#' Thanks to Garrett See for helping sort out the inconsistencies in different
+#' naming and calculating conventions.
+#' @param primary_id string identifier, usually expressed as a currency pair
+#'   'USDYEN' or 'EURGBP'
+#' @param currency string identifying the currency the exchange rate ticks in
+#' @param counter_currency string identifying the currency which the rate uses
+#'   as the base 'per 1' multiplier
+#' @param tick_size minimum price change
+#' @param identifiers named list of any other identifiers that should also be
+#'   stored for this instrument
+#' @param assign_i TRUE/FALSE. Should the instrument be assigned in the
+#'   \code{.instrument} environment? (Default TRUE)
+#' @param overwrite \code{TRUE} by default.  If \code{FALSE}, an error will
+#'   be thrown if there is already an instrument defined with the same
+#'   \code{primary_id}.
+#' @param ... any other passthru parameters
+#' @references http://financial-dictionary.thefreedictionary.com/Base+Currency
+#' @export
 currency = R6Class("currency",
                    lock_objects = FALSE,
                    public = list(
@@ -44,7 +83,7 @@ currency = R6Class("currency",
                      initialize = function(primary_id, identifiers = NULL,
                                            multiplier = 1, tick_size = .01,
                                            type = NULL, ...) {
-                       self$primary_id = primary_id
+                       self$primary_id = deparse(substitute(primary_id))
                        self$identifiers = identifiers
                        self$multiplier = multiplier
                        self$tick_size = tick_size
@@ -56,9 +95,11 @@ currency = R6Class("currency",
                        }
                      }
                    ),
-                   parent_env = instruments
+                   parent_env = instrument_all
 )
 
+#' @export
+#' @rdname instrument
 stock = R6Class("stock",
                    lock_objects = FALSE,
                    public = list(
@@ -66,14 +107,18 @@ stock = R6Class("stock",
                      identifiers = NULL,
                      multiplier = 1,
                      tick_size = .01,
+                     currency = currency$new(USD),
                      initialize = function(primary_id, identifiers = NULL,
                                            multiplier = 1, tick_size = .01,
-                                           type = NULL, ...) {
-                       self$primary_id = primary_id
+                                           type = NULL,
+                                           currency = NULL,
+                                           ...) {
+                       self$primary_id = deparse(substitute(primary_id))
                        self$identifiers = identifiers
                        self$multiplier = multiplier
                        self$tick_size = tick_size
                        self$type = type
+                       self$currency = currency
                        extra = list(...)
                        extra_names = names(extra)
                        for (i in seq_len(length((extra_names)))){
@@ -81,7 +126,7 @@ stock = R6Class("stock",
                        }
                      }
                    ),
-                   parent_env = instruments
+                   parent_env = instrument_all
 )
 
 # TODO: Add currency to stock
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # fire
+
+You can do stuff like this now
+
+```r
+test = instruments$new()
+test$addCurrency(primary_id = USD, multipler = .3, foo = "blarney")
+test$addCurrency(primary_id = EUR, multipler = 1.3, foo = "baloney")
+test$getCurrency(primary_id = EUR)
+test$addStock(primary_id = ETX, currency = USD)
+test$getStock(ETX)
+<stock>
+#  Public:
+#    clone: function (deep = FALSE) 
+#    currency: NULL #whoops!
+#    identifiers: currency, R6
+#    initialize: function (primary_id, identifiers = NULL, multiplier = 1, tick_size = 0.01, 
+#    multiplier: 1
+#    primary_id: primary_id
+#    tick_size: 0.01
+#    type: NULL
+```
